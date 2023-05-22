13df6a0dd4098a9d8bd9ed98d9dbc61ff57dd86e
Ji Young Lim <jyl1030@gmail.com>
2023-05-21 17:13:47 -0700

    initial commit

--- app.py
+++ app.py
@@ -2,34 +2,24 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from dotenv import load_dotenv  # Import the python-dotenv module
+
+load_dotenv()  # Load the environment variables from .env file
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        code_prompt = request.form["text"]  # Changed from "prompt" to "text"
         response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            engine="text-davinci-003",
+            prompt=code_prompt,
+            max_tokens=100,
+            temperature=0.2
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+    return render_template("index.html", translated_code=result)  # Changed "result" to "translated_code"
